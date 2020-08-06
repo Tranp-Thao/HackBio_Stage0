3bd6b085fcc4a8d9e3938a5adaf62467274be87e
Tranp-Thao <69023389+Tranp-Thao@users.noreply.github.com>
2020-08-06 16:58:21 +0700

    Add files via upload

---
+++ Alfrediolu.java
@@ -0,0 +1,13 @@
+class Alfrediolu
+{
+    public static void main(String args[])
+    {
+        String name = ("Alfred Akinlalu");
+        String email = ("alfrediolu@gmail.com");
+        String language = ("java");
+        String biostack = ("Drug Development");
+        String slackname = ("@alfrediolu");
+        System.out.print(name+","+email+","+language+","+biostack+","+slackname);
+        System.out.println("");
+    }
+}
---
+++ Thao.java
@@ -0,0 +1,10 @@
+public class Thao {
+    public static void main(String[] args){
+        String name = ("Tran Phuong Thao");
+        String email = ("tranpthao1093@gmail.com");
+        String language = ("java");
+        String biostack = ("drug development");
+        String slackname = ("@Thao");
+        System.out.print(name+","+email+","+language+","+biostack+","+slackname);
+    }
+}
